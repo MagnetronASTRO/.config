4f6cff75b83f101c05eb816cd70d44dde16dd22a
DominikGrudnik <dominik.grudnik@onpoint.pl>
2025-02-05 19:29:54 +0100

    [#010] upgrade to custom config based on kickstart

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,50 @@
+vim.g.have_nerd_font = true
+
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 0
+vim.g.netrw_winsize = 25
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = false
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.opt.breakindent = true
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoread = true
+
+vim.api.nvim_create_autocmd({ 'FileChangedShellPost' }, {
+  pattern = { '*' },
+  callback = function()
+    vim.notify('File changed on disk. Buffer reloaded.', vim.log.levels.INFO)
+    vim.cmd 'edit!' -- Reload the file
+  end,
+})
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,38 @@
-return {
-  -- Highlight, edit, and navigate code
+return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
-  'nvim-treesitter/nvim-treesitter-context',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'js' },
+    ensure_installed = {
+      'lua',
+	  'php',
+      'python',
+      'javascript',
+      'typescript',
+      'vimdoc',
+      'vim',
+      'regex',
+      -- 'terraform',
+      'sql',
+      'dockerfile',
+      'toml',
+      'json',
+      'java',
+      'groovy',
+      'go',
+      'gitignore',
+      'graphql',
+      'yaml',
+      'make',
+      'cmake',
+      'markdown',
+      'markdown_inline',
+      'bash',
+      'tsx',
+      'css',
+      'html',
+    },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
